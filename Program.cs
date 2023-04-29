54bface1c7ab081c1cd95a6b8e46764407545d23
MVPlauda <plaudadev@gmail.com>
2023-04-29 01:42:09 -0300

    multiplicando

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class program
+{
+    private static void Main(string[] args)
+    {
+        float num1, num2, Multiplicação;
+
+        Console.Write("Digite um numero: ");
+        num1 = float.Parse(Console.ReadLine());
+        Console.Write("Digite outro numero: ");
+        num2 = float.Parse(Console.ReadLine());
+
+        Multiplicação = num1 * num2;
+
+        Console.WriteLine($"A multiplicação entre os 2 numero é: {Multiplicação}");
+
+
+
+
+
+
+
+    }
+}
